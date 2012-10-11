606df5f1b7d63e2a73ae215233e51933d1de3f79
Simon Bouland <simon.bouland@gmail.com>
2012-10-11 16:41:54 +0200

    comment dead liine

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 	
     if (isloggedin()){
     	
-    	$featured_groups = elgg_get_entities_from_metadata(array('metadata_name' => 'featured_group', 'metadata_value' => 'yes', 'types' => 'group', 'limit' => 6, 'full_view' => FALSE));
+    	//$featured_groups = elgg_get_entities_from_metadata(array('metadata_name' => 'featured_group', 'metadata_value' => 'yes', 'types' => 'group', 'limit' => 6, 'full_view' => FALSE));
     	//$sidebar = elgg_view("groups/featured", array('featured' => $featured_groups));
     	$sidebar = elgg_view("custom_index_inria/populargroups");
    		if (is_plugin_enabled('edifice')) {
